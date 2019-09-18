a4836397e401eb742c9ca5d543ef28671a0feeab
Derric Fragosa <fderric@gmail.com>
2019-09-18 19:45:59 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,4 +1,4 @@
-=begin def find_element_index(array,value_to_find)
+ def find_element_index(array,value_to_find)
   counter = 0 
     while counter < array.length do
       if array[counter] == value_to_find
@@ -21,7 +21,7 @@ end
      max 
 end 
 
-=end
+
 
 def find_min_value(array)
   min = array[0] 
